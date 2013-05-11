c1ba5a3da83e94b5c25a78d9c1d3691cfcd9f8fb
Nikhil Jagdale <njagdale@gmail.com>
2013-05-11 16:50:52 -0700

    Added Channel 3 code

---
+++ nikhil_switch_3.h
@@ -0,0 +1,59 @@
+// This file contains the decoded values for Nikhil Switch: 3
+
+//time values
+float timeVal[] =
+{
+	440	,
+	220	,
+	460	,
+	220	,
+	460	,
+	240	,
+	440	,
+	240	,
+	100	,
+	580	,
+	440	,
+	240	,
+	440	,
+	240	,
+	440	,
+	240	,
+	100	,
+	580	,
+	440	,
+	240	,
+	100	,
+	580	,
+	440	,
+	240	,
+	100	,
+	580	,
+	460	,
+	220	,
+	100	,
+	580	,
+	120	,
+	560	,
+	120	,
+	560	,
+	120	,
+	560	,
+	460	,
+	220	,
+	460	,
+	220	,
+	700	,
+	660	,
+	120	,
+	560	,
+	120	,
+	560	,
+	120	,
+	5340,
+};
+
+//const int timeSize = 49;
+//const int timeOff = 5269; //off time in micro seconds
+//This is just a speculation still. have to figure out this number
+const int loopCount = 3; 
